3617700c580ecc302613bbd465e32be79587d91f
Alexey <alexzal235@gmail.com>
2023-12-31 05:15:14 +0400

    add rabbitmq

--- JavaProject/src/main/java/ru/zhaleykin/JavaProject.java
+++ JavaProject/src/main/java/ru/zhaleykin/JavaProject.java
@@ -2,12 +2,14 @@ package ru.zhaleykin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import ru.zhaleykin.rabbitmq.RabbitConfiguration;
 
 @SpringBootApplication
 @EnableJpaRepositories("ru.zhaleykin.repository")
+@Import(RabbitConfiguration.class)
 public class JavaProject {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JavaProject.class);
 	}
